621f68be5bb561ac20ea0c92a388a3de61359217
Marsel Narbekov <lllmarselll@gmail.com>
2024-04-22 14:44:10 +0300

    Правки по кавычкам

--- apps/services/utils.py
+++ apps/services/utils.py
@@ -18,7 +18,7 @@ def file_directory_path(instance, filename):
     Формирование директории для сохранения фото.
     Наименование фото генерируется лучайным образом.
     """
-    filename = f'{uuid4().hex[:8]}.{filename.split('.')[-1]}'
+    filename = f'{uuid4().hex[:8]}.{filename.split(".")[-1]}'
     path = (f'{settings.MEDIA_ROOT}/images/'
             f'{instance.__class__.__name__}/{instance.slug}/{filename}')
     return path
